f6b16d8349b2f534196ae7a692c4ebd40a3dc5f5
unknown <hzhan56@stevens.edu>
2021-03-10 03:50:45 +0800

    HW04 code

---
+++ HW04_hengyuanZhang.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+
+""" retrive user's repositories and store in a list """
+def retrive_repo(userId: str):
+    repoList = list()
+    if userId == '':
+        return None
+    userUrl = "https://api.github.com/users/" + userId + "/repos"
+    r = requests.get(userUrl)
+    for i in range(len(r.json())):
+        repoList.append(r.json()[i]['name'])
+    return repoList
+
+
+""" retrive user's commits in the repository """
+def retrive_commits(userId: str, repositoryName: str):
+    if userId == '':
+        return None
+    commitNumber = 0
+    repositoryUrl = "https://api.github.com/repos/" + userId + "/" + repositoryName+"/commits"
+    r = requests.get(repositoryUrl)
+    for i in range(len(r.json())):
+        if r.json()[i]['commit']:
+            commitNumber += 1
+    return commitNumber
+
+
+""" get the output """
+def outputRepo(userId: str):
+    repoList = retrive_repo(userId)
+    for i in repoList:
+        print(f"Repo: {i}  Number of commits: {retrive_commits(userId, i)}")
+
+
+outputRepo('richkempinski')
+
