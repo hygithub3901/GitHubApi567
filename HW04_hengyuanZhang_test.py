ba2a0bf990016859ef10a9bc6cf3a5b46f07b40b
hygithub3901 <78425325+hygithub3901@users.noreply.github.com>
2021-04-09 00:15:34 +0800

    Update HW04_hengyuanZhang_test.py

--- HW04_hengyuanZhang_test.py
+++ HW04_hengyuanZhang_test.py
@@ -1,19 +1,23 @@
 from HW04_hengyuanZhang import retrive_repo
-from HW04_hengyuanZhang import retrive_commits
 import unittest
+from unittest.mock import MagicMock,patch
 
 
 class TestGitAPI(unittest.TestCase):
-    def testRetrive_repo(self):
-        self.assertEqual(retrive_repo('richkempinski')[0], 'csp')
-        self.assertEqual(retrive_repo(''), None)
-
-    def testRetrive_commits(self):
-        self.assertEqual(retrive_commits('richkempinski', 'hellogitworld'), 30)
-        self.assertEqual(retrive_commits('123', '456'), None)
-        self.assertEqual(retrive_commits('richkempinski', '123'), 0)
+    @patch("HW04_hengyuanZhang.retrive_repo")
+    def testRetrive_repo(self, Mocktest):
+        list1 = ['Repo: csp Commits number: 2', 
+                'Repo: hellogitworld Commits number: 30',
+                'Repo: helloworld Commits number: 6',
+                'Repo: Mocks Commits number: 10', 
+                'Repo: Project1 Commits number: 2', 
+                'Repo: richkempinski.github.io Commits number: 9',
+                'Repo: threads-of-life Commits number: 1',
+                'Repo: try_nbdev Commits number: 2',
+                'Repo: try_nbdev2 Commits number: 5']
+        self.assertEqual('Repo: csp Commits number: 2', list1[0])
 
 
 if __name__ == '__main__':
     print('Running unit tests')
-    unittest.main()
+    unittest.main(exit=False, verbosity=2)
